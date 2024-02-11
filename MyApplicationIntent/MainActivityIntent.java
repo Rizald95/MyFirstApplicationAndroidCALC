57f2b2f92fe14df885073c0f7fe1c9b99bdb6d1d
Rizal <rizalbario3@gmail.com>
2024-02-11 07:27:34 +0700

    add intent and activity at MyApplicationIntent Folder

---
+++ MyApplicationIntent/MainActivityIntent.java
@@ -0,0 +1,17 @@
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+ 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+ 
+        val btnMoveActivity: Button = findViewById(R.id.btn_move_activity)
+        btnMoveActivity.setOnClickListener(this)
+    }
+ 
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_move_activity -> {
+            }
+        }
+    }
+}
